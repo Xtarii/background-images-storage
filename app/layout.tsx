eb566265586e864ee9a2ad0ed69449365b5e579d
Xtarii <carlalvinelias@gmail.com>
2024-11-17 21:41:36 +0100

    more testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-         <base href="nextConfig.basePath" />
+         <base href={nextConfig.basePath} />
       </head>
 
       <body
