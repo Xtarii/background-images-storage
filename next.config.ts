85faf70ac6ccaf98023db7b8393628247c9c1d5b
Xtarii <carlalvinelias@gmail.com>
2024-11-17 20:20:18 +0100

    Test

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,10 @@ import type { NextConfig } from "next";
 
 
 
-const isProduction = process.env.NODE_ENV === "production";
-
-
-
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: isProduction ? "/background-images-storage" : "",
-  basePath: isProduction ? "/background-images-storage" : "",
+  assetPrefix: "/background-images-storage",
+  basePath: "/background-images-storage",
 
   output: "export",
   reactStrictMode: true
