a8f1b99ac13e6b5369d072964f15f645a0ba1dbe
Xtarii <carlalvinelias@gmail.com>
2024-11-17 19:43:39 +0100

    added production and assets

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
+
+
+const isProduction = process.env.NODE_ENV === "production";
+
+
+
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: "/background-images-storage",
+  assetPrefix: isProduction ? "/background-images-storage" : "",
+  basePath: isProduction ? "/background-images-storage" : "",
+
   output: "export",
   reactStrictMode: true
 };
