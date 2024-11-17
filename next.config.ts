69d4d668e56f4e2b5067f94a34aa62f0e2a8eb65
Xtarii <carlalvinelias@gmail.com>
2024-11-17 20:09:58 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -8,8 +8,8 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  assetPrefix: isProduction ? "https://xtarii.github.io/background-images-storage/" : "",
-  basePath: isProduction ? "https://xtarii.github.io/background-images-storage/" : "",
+  assetPrefix: isProduction ? "/background-images-storage/" : "",
+  basePath: isProduction ? "/background-images-storage/" : "",
 
   output: "export",
   reactStrictMode: true
