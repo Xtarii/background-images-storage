ef8f33d5f1b7b9159fba04291948794573469271
Xtarii <carlalvinelias@gmail.com>
2024-11-18 22:15:29 +0100

    Test again

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // basePath: "/background-images-storage",
+  basePath: "/background-images-storage",
 
   output: "export",
   reactStrictMode: true
