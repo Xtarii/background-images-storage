1ebcf2ab596bf237ed79def5a67bc3374d163b03
Xtarii <carlalvinelias@gmail.com>
2024-11-20 19:24:40 +0100

    Image adding form

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -18,10 +18,7 @@ export default function Create() : ReactElement {
 
 
 
-    return(<div>
-        <h1>Create New Item</h1>
-
-
+    return(<div className="mt-16">
         <form ref={ref} onSubmit={async (e: FormEvent<HTMLFormElement>) => {
             e.preventDefault();
 
@@ -31,55 +28,42 @@ export default function Create() : ReactElement {
             const image = data.get("image") as File;
             const name = data.get("name")?.toString();
             const tagID = data.get("tag")?.toString();
-
             if(tagID === undefined || name === undefined || image === null) return;
 
-
-            // Get Image Data
+            // Store Data
             const arrayBuffer = await image.arrayBuffer();
-            // const buffer = new Uint8Array(arrayBuffer);
-
-            // Create new Item
             const uuid = v7();
-            const item = {
-                title: name,
-                UUID: uuid,
-
-                image: arrayBuffer
-            };
-
-            console.log(item); // DEBUG
-
-            // Push Item to Github-pages
-
-            /**
-             * There is gonna be a super file that contains
-             * the tags,
-             *
-             * every tag has a sub-folder with a file: tag.json
-             * that file will tell what files is located in that folder
-             *
-             *
-             * Image URLS are located as:
-             * UUID_TAG / imgs / CUSTOM_NAME - UUID_IMAGE . jpg
-             */
-            ///
-
-
-
-            const tag = await getTags(tagID);
-            if(!tag) return; // Stops if there is no tag found
-
-            // storeItem(tagID, item);
+            await storeItem(tagID, { title: name, UUID: uuid, image: arrayBuffer });
 
             // Redirect to other page
             ref.current?.reset(); // Clears Form
-        }}>
-            <input type="text" name="tag" required />
-            <input type="text" name="name" required />
-            <input type="file" name="image" required />
+        }}
 
-            <input type="submit" value="Create Item" />
+        className="max-w-sm mx-auto"
+        >
+            <div className="mb-5">
+                <label htmlFor="tag" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image Tag</label>
+                <input type="text" id="tag" name="tag" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="guys" required />
+            </div>
+            <div className="mb-5">
+                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image Name</label>
+                <input type="text" id="name" name="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            </div>
+
+            <div className="flex items-center justify-center w-full">
+                <label htmlFor="image" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF</p>
+                    </div>
+                    <input id="image" name="image" type="file" className="hidden" />
+                </label>
+            </div>
+
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Upload</button>
         </form>
     </div>);
 }
