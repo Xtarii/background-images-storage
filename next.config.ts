530071824a3d371d60b21613ddd0400f003009c3
Xtarii <carlalvinelias@gmail.com>
2024-11-22 11:00:21 +0100

    test with env var

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   // basePath: process.env.NODE_ENV === "production" ? "/background-images-storage" : "",
 
+  env: {
+    MODE: process.env.MODE
+  },
+
   output: "export",
   reactStrictMode: true
 };
