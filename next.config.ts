744c83ad3fd13c8bb419cc134349e90d7109f7b9
Xtarii <carlalvinelias@gmail.com>
2024-11-22 11:48:41 +0100

    fixed config for nextJS - github pls work now or I will sue my mother

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: process.env.NEXT_PUBLIC_NODE_ENV === "production" ? "/background-images-storage" : "",
-
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL : process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_SECRET_KEY : process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY
-  },
-
   output: "export",
   reactStrictMode: true
 };
