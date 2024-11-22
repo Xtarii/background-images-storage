6cbfba9c6ee2e8f0323ad1d7ba473b0bcd8b6be6
Xtarii <carlalvinelias@gmail.com>
2024-11-22 11:17:37 +0100

    Github var test

--- utils/api/routes.ts
+++ utils/api/routes.ts
@@ -2,19 +2,13 @@
 ///
 
 import { createClient } from "@supabase/supabase-js";
-import path from "path";
 
 
-
-/**
- * API Routes Base Path
- */
-export const base = "https://uyfiatbchvzyavoatgyk.supabase.co";
 
 /**
  * Client Connection
  */
 export const supabase = createClient(
-    base,
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5ZmlhdGJjaHZ6eWF2b2F0Z3lrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIwMDQ5OTYsImV4cCI6MjA0NzU4MDk5Nn0.NILas5jUlZMYWX70sUkrrISd3Iwcf1dIyPbyzuMMY8c"
+    process.env.SUPABASE_URL ? process.env.SUPABASE_URL : "",
+    process.env.SUPABASE_SECRET_KEY ? process.env.SUPABASE_SECRET_KEY : ""
 );
