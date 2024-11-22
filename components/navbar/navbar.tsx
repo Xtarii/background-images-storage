8ca9bf70b8caa9da85b7473749043ee5a3c9a963
Xtarii <carlalvinelias@gmail.com>
2024-11-22 11:05:54 +0100

    new test

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -10,9 +10,6 @@ export default function Navbar() : ReactElement {
         post: path.join(base, "/post")
     }
 
-    console.log(base)
-    console.log(process.env.MODE)
-
 
 
     return(<nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
