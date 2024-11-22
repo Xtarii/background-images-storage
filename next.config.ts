d47b67a7a161f6d766da6d2800ab34b318e7d45f
Xtarii <carlalvinelias@gmail.com>
2024-11-22 11:30:03 +0100

    Exporting environment from Nextjs

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   // basePath: process.env.NODE_ENV === "production" ? "/background-images-storage" : "",
 
+  env: {
+    SUPABASE_URL : process.env.SUPABASE_URL,
+    SUPABASE_SECRET_KEY : process.env.SUPABASE_SECRET_KEY
+  },
+
   output: "export",
   reactStrictMode: true
 };
