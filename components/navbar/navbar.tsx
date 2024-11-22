bf1df92d170f729246608c2de64cd2d595b15cb7
Xtarii <carlalvinelias@gmail.com>
2024-11-22 10:50:16 +0100

    another fricking test

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -4,7 +4,7 @@ import { ReactElement } from "react";
 
 
 export default function Navbar() : ReactElement {
-    const base = process.env.NODE_ENV === "production" ? "https://xtarii.github.io/background-images-storage/" : ""
+    const base = process.env.MODE === "production" ? "https://xtarii.github.io/background-images-storage/" : ""
     const links = {
         home: path.join(base, "/"),
         post: path.join(base, "/post")
